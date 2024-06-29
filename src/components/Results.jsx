5c2bfdd7820e129e3cd802de1e3d36c5425c24af
heppaat <pali.herrer@gmail.com>
2024-06-29 13:08:47 +0200

    calculate logic and render v1.0

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,13 +1,43 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 const Results = ({ userInput }) => {
-  const result = calculateInvestmentResults(userInput);
-  console.log(result);
+  const resultsData = calculateInvestmentResults(userInput);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
 
   return (
-    <div>
-      <p>Hello here are the results...</p>
-    </div>
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((data, index) => {
+          const totalInterest =
+            data.valueEndOfYear -
+            data.annualInvestment * data.year -
+            initialInvestment;
+
+          const totalAmountInvested = data.valueEndOfYear - totalInterest;
+          return (
+            <tr key={index}>
+              <td>{data.year}</td>
+              <td>{formatter.format(data.valueEndOfYear)}</td>
+              <td>{formatter.format(data.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
